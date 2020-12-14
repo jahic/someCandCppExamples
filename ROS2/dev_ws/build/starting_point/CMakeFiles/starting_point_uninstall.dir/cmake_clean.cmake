0dedbf7dea56e3ae266773d5381ce90c2e1db41e
Arian Ajdari <arianajdari23@gmail.com>
2020-12-15 00:02:54 +0100

    Updated CMakeLists.txt

--- ROS2/dev_ws/build/starting_point/CMakeFiles/starting_point_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/starting_point_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/starting_point_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
